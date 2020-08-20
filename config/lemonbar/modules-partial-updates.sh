84d10910b651df6b825cb9cb00502395a8201074
Sako Hartounian <sakohartounian@yahoo.com>
2020-08-20 16:16:23 +0000

    colors moved to variables in lemonbar modules

--- config/lemonbar/modules-partial-updates.sh
+++ config/lemonbar/modules-partial-updates.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 
+#colors
+RED="#bf616a"
+GREEN="#a3be8c"
+YELLOW="#ebcb8b"
+BLUE="#81a1c1"
+MAGENTA="#b48ead"
+CYAN="#88c0d0"
+
+FGCOLOR="#2e3440"
+
 PANEL_FIFO=/tmp/panel-fifo
 
 [ -e $PANEL_FIFO ] && rm $PANEL_FIFO 
@@ -16,13 +26,7 @@ Desktops() {
 
 ActiveWindow() {
 	while true; do
-		len=$(echo -n "$(xdotool getwindowfocus getwindowname)" | wc -m)
-		max_len=70
-		if [ "$len" -gt "$max_len" ];then
-			echo "ACTIVE_WINDOW $(xdotool getwindowfocus getwindowname | cut -c 1-$max_len)..."
-		else
-			echo "ACTIVE_WINDOW $(xdotool getwindowfocus getwindowname)" 
-		fi
+		echo "ACTIVE_WINDOW $(xdotool getwindowfocus getwindowname)"
 		sleep 1
 	done
 }
@@ -30,7 +34,7 @@ ActiveWindow() {
 Clock() {
 	while true; do
 		TIME=$(date "+%H:%M:%S")
-		echo -e "CLOCK %{B#81a1c1}%{F#2e3440} \uf017 ${TIME} %{B-}%{F-}" 
+		echo -e "CLOCK %{B$BLUE}%{F#2e3440} \uf017 ${TIME} %{B-}%{F-}" 
 		sleep 1;
 	done
 }
@@ -42,27 +46,32 @@ Battery() {
 
 		if [[ "$(acpi | awk '{print $3}')" == 'Charging,' ]]; then
         		ICON="\uf5e7"
+			BGCOLOR=$GREEN
 		elif [[ $BAT -ge 90 ]]; then
 			ICON="\uf240"
+			BGCOLOR=$GREEN
 		elif [[ $BAT -ge 75 ]]; then	
 			ICON="\uf241"
+			BGCOLOR=$YELLOW
 		elif [[ $BAT -ge 50 ]]; then
 			ICON="\uf242"
+			BGCOLOR=$YELLOW
 		elif [[ $BAT -ge 25 ]]; then
 			ICON="\uf243"
+			BGCOLOR=$RED
 		else
 			ICON="\uf244"
+			BGCOLOR=$RED
 		fi
 
-		echo -e "BATTERY %{B#a3be8c}%{F#2e3440} $ICON $BATPERC %{B-}%{F-}" 
-		sleep 1
+		echo -e "BATTERY %{B$BGCOLOR}%{F$FGCOLOR} $ICON $BATPERC %{B-}%{F-}" 
+		sleep 2
 	done 
 }
 
 Sound() {
 	while true; do
-		BGCOLOR="#ebcb8b"
-		FGCOLOR="#2e3440"
+		BGCOLOR=$YELLOW
 		NOTMUTED=$( amixer sget Master | grep "\[on\]" )
 		VOL=$(awk -F"[][]" 'NR==6{ print $2 }' <(amixer sget Master) | sed 's/%//g')
 		if [[ ! -z $NOTMUTED ]] ; then
@@ -84,11 +93,11 @@ Sound() {
 Wifi() {
 	while true; do
 		STATE=$(connmanctl state | awk 'NR == 1 {print $3}')
-		BGCOLOR="#b48ead"	
+		BGCOLOR=$MAGENTA	
 		if [[ $STATE == "online" ]]; then
-			echo "WIFI %{B$BGCOLOR}%{F#2e3440}  $(connmanctl services | awk 'NR == 1 {print $2}') %{B-}%{F-}" 
+			echo "WIFI %{B$BGCOLOR}%{F$FGCOLOR}  $(connmanctl services | awk 'NR == 1 {print $2}') %{B-}%{F-}" 
 		else
-			echo "WIFI %{B$BGCOLOR}%{F#2e3440}睊Not Connected %{B-}%{F-}" 
+			echo "WIFI %{B$BGCOLOR}%{F$FGCOLOR}睊Not Connected %{B-}%{F-}" 
 		fi
 	
 		sleep 10;
@@ -123,6 +132,6 @@ while read -r line; do
 			fn_battery="${line#BATTERY }"
 			;;
 	esac
-	printf "%s\n" "%{l}$fn_desktop  $(echo $fn_active_window | sed 's/ACTIVE_WINDOW//g' )  %{r}$fn_sound$fn_wifi$fn_clock$fn_battery" 
+	printf "%s\n" "%{l}$fn_desktop  %{c}$(echo $fn_active_window | sed 's/ACTIVE_WINDOW//g' )  %{r}$fn_sound$fn_wifi$fn_clock$fn_battery" 
 done < $PANEL_FIFO 
 
