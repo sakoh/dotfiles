575fd692a5ebad75aa1a67181826dbf0399f1e84
Sako Hartounian <sakohartounian@yahoo.com>
2020-12-05 02:02:36 -0800

    lemonbar and alacrity font now Fira

--- config/lemonbar/launch.sh
+++ config/lemonbar/launch.sh
@@ -1,9 +1,6 @@
 $HOME/.config/lemonbar/modules-partial-updates.sh -n | lemonbar \
 	-f "Font Awesome 5 Free Solid"-14 \
-	-f "Ubuntu Mono derivative Powerline Bold"-14 \
-	-f "font-awesome-extension"-14 \
-	-f"Material Design Icons"-14 \
-	-f "Material Icons Regular"-14 \
-	-g 1920x35+0+0 \
+	-f "Fira Mono Nerd Font"-12 \
+	-g 1920x45+0+0 \
 	-B"#101726" \
 	-F"#d8dee9" | $SHELL
