d14c2124f444ed12eb8d52b77105ceb95debf5d6
Sako Hartounian <sakohartounian@yahoo.com>
2020-12-05 09:42:53 -0800

    font and bar sizes adjusted

--- config/lemonbar/launch.sh
+++ config/lemonbar/launch.sh
@@ -1,6 +1,6 @@
 $HOME/.config/lemonbar/modules-partial-updates.sh -n | lemonbar \
 	-f "Font Awesome 5 Free Solid"-14 \
 	-f "Fira Mono Nerd Font"-12 \
-	-g 1920x45+0+0 \
+	-g 1920x35+0+0 \
 	-B"#101726" \
 	-F"#d8dee9" | $SHELL
