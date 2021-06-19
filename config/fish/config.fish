63d0b14007aca370c5aa6fc62b8c6e185f293bf2
Sako Hartounian <sakohartounian@yahoo.com>
2021-06-18 23:41:37 -0700

    fish paths cleaned up

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 starship init fish | source
 
-set PATH $PATH /home/sakoh/.local/bin /home/sakoh/.cargo/bin
+fish_add_path /home/sakoh/.local/bin
+fish_add_path /home/sakoh/.cargo/bin
 
 if status is-login
     if test -z "$tty" -a "$XDG_VTNR" = 1
