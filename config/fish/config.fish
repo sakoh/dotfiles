303e8e47c236e558e01d59420d7f0bbd5c82dbf8
Sako Hartounian <sakohartounian@yahoo.com>
2021-06-18 22:45:12 -0700

    on fish login, export paths and startx

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,9 @@
 starship init fish | source
 
+set PATH $PATH /home/sakoh/.local/bin /home/sakoh/.cargo/bin
+
+if status is-login
+    if test -z "$tty" -a "$XDG_VTNR" = 1
+        startx
+    end
+end
