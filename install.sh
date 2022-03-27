809d16c7d25c2683c4440e48b9c30b40af4b1fb7
Sako Hartounian <sakohartounian@yahoo.com>
2022-03-27 10:14:03 -0700

    dwmblocks added to install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ cp -r config/* $HOME/.config/
 cp -r Pictures $HOME/
 #cp etc/pulse/default.pa /etc/pulse/default.pa
 #cp etc/xdg/picom.conf /etc/xdg/picom.conf
+mkdir $HOME/.fonts
 cp fonts/* $HOME/.fonts/
 cp zprofile $HOME/.zprofile
 cp zshrc $HOME/.zshrc
 cp xinitrc $HOME/.xinitrc
 cp p10k.zsh $HOME/.p10k.zsh
 cp tmux.conf $HOME/.tmux.conf
+cp dwmblocks $HOME/.dwmblocks
